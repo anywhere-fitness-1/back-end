b46e1e134eae8e01796ea7425ddfdcdeca878c80
chad-scanlon <59572157+chad-scanlon@users.noreply.github.com>
2020-08-20 09:52:15 -0400

    added production in knexfile

--- knexfile.js
+++ knexfile.js
@@ -24,4 +24,29 @@ module.exports = {
       },
     },
   },
+  production: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./data/auth.db3",
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    useNullAsDefault: true,
+
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
 };
