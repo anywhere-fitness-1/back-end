e548b874f6af719f89a361e58f069c94e3d8ab93
chad-scanlon <59572157+chad-scanlon@users.noreply.github.com>
2020-08-26 23:00:09 -0400

    knex config

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,5 +1,5 @@
-const knex = require("knex");
-const knexConfig = require("../knexfile.js");
 const environment = process.env.NODE_ENV || "development";
+const knexConfig = require("../knexfile.js")[environment];
+const knex = require("knex")(knexConfig);
 
-module.exports = knex(knexConfig[environment]);
+module.exports = knex;
